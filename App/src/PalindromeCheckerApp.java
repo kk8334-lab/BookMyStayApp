8a446e51a0899a966a7d9857083643778298f696
SAKET-2005 <sb9896@srmist.edu.in>
2026-02-26 15:17:36 +0530

    SAKET-2005 Added UseCase-1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+/*
+================================================================================================================
+MAIN CLASS - UseCase1PalindromeApp
+================================================================================================================
+
+Use Case 1: Application Entry and Welcome Message
+
+Description:
+This class represents the entry point of the
+Palindrome Checker Management System
+
+At this stage, the application:
+- Starts execution from the main method
+- Displays a welcome message
+- Shows application Version
+
+No palindrome logic is implemented yet
+
+The goal is to establish a clear startup flow.
+
+@author SAKET-2005
+@version 1.0
+ */
+
+public class PalindromeCheckerApp
+{
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to The Pallindrome Checker");
+        System.out.println("Author: SAKET-2005");
+        System.out.println("Version: 1.0");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PalindromeCheckerApp
+## Running Procedure
+Run the Program PallindromeCheckerApp.java by first compiling and then using the run button.
+## Flow of Project
+Program Starts
+JVM invokes the main() method.
+Application name is displayed
+Application version is displayed
+Program continues to next use case or exits
+## Objective
+To implement a Pallindrome Checker to find if a given input is Pallindrome or not using the most efficient solution.
